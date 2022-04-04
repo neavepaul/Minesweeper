51216064a7a43d644e793dabad45a7401865ba41
Neave Paul <neavenmoira@yahoo.com>
2022-04-04 18:01:15 +0530

    AI

--- minesweeper.py
+++ minesweeper.py
@@ -97,3 +97,120 @@ class Sentence():
             self.cells.remove(cell)
             return 1
         return 0
+
+
+class MinesweeperAI():
+
+    def __init__(self, height, width):
+
+        # Set initial height and width
+        self.height = height
+        self.width = width
+
+        # Keep track of which cells have been clicked on
+        self.moves_made = set()
+
+        # Keep track of cells known to be safe or mines
+        self.mines = set()
+        self.safes = set()
+
+        # Set of sentences about the game known to be true
+        self.knowledge = []
+
+    def mark_mine(self, cell):
+        counter = 0
+        self.mines.add(cell)
+        for sentence in self.knowledge:
+            counter += sentence.mark_mine(cell)
+        return counter
+
+    def mark_safe(self, cell):
+        counter = 0
+        self.safes.add(cell)
+        for sentence in self.knowledge:
+            counter += sentence.mark_safe(cell)
+        return counter
+
+    def add_knowledge(self, cell, count):
+        self.moves_made.add(cell)
+
+        self.mark_safe(cell)
+
+        i, j = cell
+        neighbors = set()
+        for a in range(max(0, i-1), min(i+2, self.height)):
+            for b in range(max(0, j-1), min(j+2, self.width)):
+                if (a, b) != (i, j):
+                    neighbors.add((a, b))
+        self.knowledge.append(Sentence(neighbors, count))
+
+        self.update_self_and_sentences()
+
+
+        guesses = self.new_guesses()
+
+        while guesses:
+            for sentence in guesses:
+                self.knowledge.append(sentence)
+
+            self.update_self_and_sentences()
+
+
+            guesses = self.new_guesses()
+
+    
+    def make_safe_move(self):
+        for move in self.safes:
+            if move not in self.moves_made and move not in self.mines:
+                return move
+
+        return None
+
+    def make_random_move(self):
+        for i in range(0, self.height):
+            for j in range(0, self.width):
+                move = (i, j)
+                if move not in self.mines and move not in self.moves_made:
+                    return move
+
+        return None
+
+
+    def new_guesses(self):
+        guesses = []
+        removals = []
+
+        for sentence1 in self.knowledge:
+            if sentence1.cells == set():
+                removals.append(sentence1)
+                continue
+            for sentence2 in self.knowledge:
+                if sentence2.cells == set():
+                    removals.append(sentence2)
+                    continue
+                if sentence1 != sentence2:
+                    if sentence2.cells.issubset(sentence1.cells):
+                        diff_cells = sentence1.cells - sentence2.cells
+                        diff_count = sentence1.count - sentence2.count
+                        new_guess = Sentence(diff_cells, diff_count)
+                        if new_guess not in self.knowledge:
+                            guesses.append(new_guess)
+
+        self.knowledge = [x for x in self.knowledge if x not in removals]
+        return guesses
+
+    def update_self_and_sentences(self):
+        counter = 1
+        while counter:
+            counter = 0
+            for sentence in self.knowledge:
+                for cell in sentence.known_safes():
+                    self.mark_safe(cell)
+                    counter += 1
+                for cell in sentence.known_mines():
+                    self.mark_mine(cell)
+                    counter += 1
+            for cell in self.safes:
+                counter += self.mark_safe(cell)
+            for cell in self.mines:
+                counter += self.mark_mine(cell)

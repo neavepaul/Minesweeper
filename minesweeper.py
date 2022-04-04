2041c88fad8174206cdbbd5c883ad19e6cc84979
Neave Paul <neavenmoira@yahoo.com>
2022-04-04 17:54:25 +0530

    Memory of the AI

--- minesweeper.py
+++ minesweeper.py
@@ -4,7 +4,6 @@ import random
 class Minesweeper():
 
     def __init__(self, height, width, mines):
-
         # Set initial width, height, and number of mines
         self.height = height
         self.width = width
@@ -60,3 +59,41 @@ class Minesweeper():
 
     def won(self):
         return self.mines_found == self.mines
+
+
+class Sentence():
+
+    def __init__(self, cells, count):
+        self.cells = set(cells)
+        self.count = count
+
+    def __eq__(self, other):
+        return self.cells == other.cells and self.count == other.count
+
+    def __str__(self):
+        return f"{self.cells} = {self.count}"
+
+    def known_mines(self):
+        if len(self.cells) == self.count:
+            return set(self.cells)
+        else:
+            return set()
+
+    def known_safes(self):
+        if self.count == 0:
+            return set(self.cells)
+        else:
+            return set()
+
+    def mark_mine(self, cell):
+        if cell in self.cells:
+            self.cells.remove(cell)
+            self.count -= 1
+            return 1
+        return 0
+
+    def mark_safe(self, cell):
+        if cell in self.cells:
+            self.cells.remove(cell)
+            return 1
+        return 0
